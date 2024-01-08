f7a655bfdb0e949c57ed90e2c072106afaecfe98
Yuristian <yuristian.ardianto@gmail.com>
2024-01-08 22:53:29 +0700

    add views for property types and datatables

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -44,6 +44,7 @@ License: For each use you must have a valid license purchased only from above li
 
   <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+  <link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
 </head>
 <body>
 	<div class="main-wrapper">
@@ -80,6 +81,10 @@ License: For each use you must have a valid license purchased only from above li
 	<script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
 	<script src="{{ asset('backend/assets/js/template.js') }}"></script>
 	<!-- endinject -->
+
+    <script src="{{ asset('backend/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/data-table.js') }}"></script>
 
 	<!-- Custom js for this page -->
     <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
